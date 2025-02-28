f2fd8df95ab61bc4b911da210c1fa2f782b1bed3
Max Klenov <maxklenow@gmail.com>
2025-02-28 08:20:11 -0800

    Update - Social.jsx

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,42 @@
+const social = () => {
+    const socialLinks = [
+        {
+            href: "https://api.whatsapp.com/send?phone=79964329699&text=%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%90%D0%BD%D0%BE%D0%BD%D0%B8%D0%BC",
+            className: "bx bxl-whatsapp",
+            ariaLabel: "Contact via WhatsApp",
+            title: "Связаться через Whatsapp",
+        },
+        {
+            href: "https://t.me/tgxzz",
+            className: "uil uil-telegram",
+            ariaLabel: "Contact via Telegram",
+            title: "Связаться через Telegram",
+        },
+        {
+            href: "https://github.com/maxim-klenov",
+            className: "uil uil-github-alt",
+            ariaLabel: "View GitHub Profile",
+            title: "Перейти в Github",
+        },
+    ];
+
+    return (
+        <div className="home__social">
+            {socialLinks.map((link, index) => (
+                <a
+                    key={index}
+                    href={link.href}
+                    title={link.title}
+                    className="home__social-icon"
+                    target="_blank"
+                    aria-label={link.ariaLabel}
+                    rel="noreferrer"
+                >
+                    <i className={link.className} />
+                </a>
+            ))}
+        </div>
+    );
+};
+
+export default social;
