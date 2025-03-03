94f9692b1e3421f059ed1f132e0a5716da292032
Max Klenov <maxklenow@gmail.com>
2025-03-02 23:29:06 -0800

    update - semantic elements

--- src/components/home/ScrollDown.jsx
+++ src/components/home/ScrollDown.jsx
@@ -2,12 +2,12 @@ import "./home.css";
 
 const ScrollDown = () => {
     return (
-        <div className="home__scroll">
+        <nav className="home__scroll">
             <a href="#about" className="home__scroll-button button--flex">
                 <span className="home__scroll-name">Посмотреть резюме</span>
                 <i className="uil uil-arrow-down home__scroll-arrow"></i>
             </a>
-        </div>
+        </nav>
     );
 };
 
