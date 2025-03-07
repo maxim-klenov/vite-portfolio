39fdee65ce4a2c2900f6657e0ab3c7713ba0fbaa
Max Klenov <maxklenow@gmail.com>
2025-03-07 10:43:33 +0300

    Update - svg

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,20 +1,21 @@
+import { ReactSVG } from 'react-svg';
 const social = () => {
     const socialLinks = [
         {
             href: "https://api.whatsapp.com/send?phone=79964329699&text=%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%90%D0%BD%D0%BE%D0%BD%D0%B8%D0%BC",
-            className: "bx bxl-whatsapp",
+            src: "/svg/whatsapp.svg",
             ariaLabel: "Contact via WhatsApp",
             title: "Связаться через Whatsapp",
         },
         {
             href: "https://t.me/tgxzz",
-            className: "uil uil-telegram",
+            src: "/svg/telegram.svg",
             ariaLabel: "Contact via Telegram",
             title: "Связаться через Telegram",
         },
         {
             href: "https://github.com/maxim-klenov",
-            className: "uil uil-github-alt",
+            src: "/svg/github.svg",
             ariaLabel: "View GitHub Profile",
             title: "Перейти в Github",
         },
@@ -32,7 +33,7 @@ const social = () => {
                     aria-label={link.ariaLabel}
                     rel="noopener noreferrer"
                 >
-                    <i className={link.className} />
+                    <ReactSVG src={link.src} />
                 </a>
             ))}
         </aside>
