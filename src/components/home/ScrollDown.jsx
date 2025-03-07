b745a3e8dd4548fddad1d7c0c39096fd33f0121f
Max Klenov <maxklenow@gmail.com>
2025-03-07 10:43:15 +0300

    Update - svg

--- src/components/home/ScrollDown.jsx
+++ src/components/home/ScrollDown.jsx
@@ -1,11 +1,12 @@
 import "./home.css";
+import { ReactSVG } from 'react-svg';
 
 const ScrollDown = () => {
     return (
         <nav className="home__scroll">
             <a href="#about" className="home__scroll-button button--flex">
                 <span className="home__scroll-name">Посмотреть резюме</span>
-                <i className="uil uil-arrow-down home__scroll-arrow"></i>
+                <ReactSVG src="/svg/arrow-down.svg" className="home__scroll-arrow"/>
             </a>
         </nav>
     );
